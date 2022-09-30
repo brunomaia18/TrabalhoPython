82531e79261a04cb00d4e69820964531b94b32bc
Bruno_maia <67295699+brunomaia18@users.noreply.github.com>
2022-09-30 08:30:52 -0300

    Update atv01-perimetro.py

--- atv01-perimetro.py
+++ atv01-perimetro.py
@@ -1,11 +1,9 @@
 ##definindo como vai começar 
-lados = 0
-soma = 0
-while lados < 4:
-    #Perguntando os 4 lados do triangulo
-    lado1 = float(input("Digite o lado do Retângulo : "))
-    #vai somar o lados que começa com zero a cada repetiçao
-    lados += 1
-    #Somando cada lado dito pelo cliente
-    soma += lado1
-print(f"A soma do perimetro é {soma}")
+lado1 = float(input("Digite o lado do Retângulo : "))
+lado2 = float(input("Digite o lado do Retângulo : "))
+perimetro = lado1+lado1+lado2+lado2
+area = lado1*lado2
+
+print(f"O perimetro do Retangulo é {perimetro}")
+
+print(f"A area do Retangulo é {area}")
