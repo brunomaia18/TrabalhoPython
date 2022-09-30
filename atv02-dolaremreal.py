e58992aed76cc10c64b5e56c338a833f8f08d582
Bruno_maia <67295699+brunomaia18@users.noreply.github.com>
2022-09-30 09:50:32 -0300

    Update atv02-dolaremreal.py

--- atv02-dolaremreal.py
+++ atv02-dolaremreal.py
@@ -1,9 +1,11 @@
 #Perguntar quantos dolar quer converter em real
 valor = float(input("Quantos dolares você quer converter em real: "))
-converter = valor * 5.26
+cota = 5.65
+real = valor * cota
 
 if valor >= 100:
-    desconto= valor * 5.78
-    print(f"Como você vai converter mais de 100 dolares voce tem um total de 10% de desconto em vez de receber {converter} ira receber R${desconto}")
+    desconto= valor * (cota + (cota*0.1))
+    print(f"Como você vai converter mais de 100 dolares voce tem um total de 10% de desconto em vez de receber {real} ira receber R${desconto:,.2f}")
 else:
-    print(f"Você vai converter ${valor} em R${converter:,.2f}")
+    
+    print(f"Você vai converter ${valor} em R${real:,.2f}")
