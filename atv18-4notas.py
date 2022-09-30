a5f7517fb1ea6ee559d0b2d8ce68ed9f122428cd
Bruno_maia <67295699+brunomaia18@users.noreply.github.com>
2022-09-30 09:36:12 -0300

    Add files via upload

---
+++ atv18-4notas.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Digite a sua Nota: "))
+nota2 = float(input("Digite a sua Nota: "))
+nota3 = float(input("Digite a sua Nota: "))
+nota4 = float(input("Digite a sua Nota: "))
+
+media = (nota1+nota2+nota3+nota4) / 4
+
+print(f"Suas medias fora {nota1} {nota2} {nota3} {nota4} e a sua media total foi {media}")
