57a88565ece2a5175b270d8a92d84aa5f1b0f78e
Bruno_maia <67295699+brunomaia18@users.noreply.github.com>
2022-10-06 21:23:03 -0300

    Update atv05-tabuada.py

--- atv05-tabuada.py
+++ atv05-tabuada.py
@@ -1,6 +1,7 @@
 numero = int(input("Digite um numero para ver a tabuada : "))
 count = 0
-for c in range(0,11):
+print(f"{numero} X 0 = 0")
+for c in range(1,11):
     multi = numero*c
     count = count + 1
     print("{} X {} = {}".format(numero,count,multi)) 
